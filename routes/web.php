ebc1e6c7c19d2e0aec281faf22622b31af685b1c
James Dick <jimandjo.geo@yahoo.com>
2025-02-23 12:10:36 -0500

    Auto loading namespaces is working

--- routes/web.php
+++ routes/web.php
@@ -1,43 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-Use Illuminate\Support\Arr;
-
-$jobs = [
-    [
-        'id' => 1,
-        'title' => 'Director',
-        'salary' => '50,000',
-    ],
-    [   'id' => 2,
-        'title' => 'Teacher',
-        'salary' => '40,000',
-    ],
-    [
-        'id' => 3,
-        'title' => 'Programmer',
-        'salary' => '10,000',
-    ]
-    ];
+use App\Models\Job;
 
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () use ($jobs) {
+Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => $jobs
+        'jobs' => Job::all()
       ]);
     
 });
 
-Route::get('/jobs/{id}', function ($id) use($jobs) {
+Route::get('/jobs/{id}', function ($id) {
         
-       $job = Arr::first($jobs, fn($job) => $job['id'] == $id
-        );
+      $job = Job::find($id);
             
-
     return view('job', ['job'=> $job]);
 });
 
