01fe80e6a968d495af6097a09ad885d19959654b
James Dick <jimandjo.geo@yahoo.com>
2025-04-13 22:16:43 -0400

    added CRUD to the site

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
 
+
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs/create', function() {
-    return view('jobs.create');
-});
-
+//Index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
@@ -20,7 +18,13 @@ Route::get('/jobs', function () {
       ]);
     
 });
+// create a job
+Route::get('/jobs/create', function() {
+    return view('jobs.create');
+});
 
+
+//Show a job
 Route::get('/jobs/{id}', function ($id) {
         
       $job = Job::find($id);
@@ -28,6 +32,7 @@ Route::get('/jobs/{id}', function ($id) {
     return view('jobs.show', ['job'=> $job]);
 });
 
+//Store a job
 Route::post('/jobs', function() {
     request()->validate([
         'title' => ['required', 'min:3'],
@@ -35,12 +40,53 @@ Route::post('/jobs', function() {
     ]);
     
 
+
     Job::create([
         'title' => request('title'),
         'salary' => request('salary'),
         'employer_id' => 1
     ]);
 
+    return redirect('/jobs');
+});
+
+//Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+        
+    $job = Job::find($id);
+          
+  return view('jobs.edit', ['job'=> $job]);
+});
+
+//update
+Route::patch('/jobs/{id}', function ($id) {
+        
+    //validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+    //authorize(on hold...)
+    //update
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title'=>request('title'),
+        'salary'=>request('salary'),
+    ]);
+    
+    //redirect to the job page
+    return redirect('/jobs/' . $job->id);
+
+});
+
+//Destroy
+Route::delete('/jobs/{id}', function ($id) {
+    //authorize(on hold...)
+
+    //delete
+    Job::findOrFail($id)->delete();   
+    //redirect
     return redirect('/jobs');
 });
 
